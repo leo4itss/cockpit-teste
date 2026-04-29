df1eee12333a875b95d63aee24c32789fe90dfa9
leonardolins <leonardolins@users.noreply.github.com>
2026-04-29 15:52:54 -0300

    auto: update NewSolutionSheet.tsx

--- src/components/NewSolutionSheet.tsx
+++ src/components/NewSolutionSheet.tsx
@@ -360,6 +360,7 @@ export function NewSolutionSheet({
         componentes={componentes}
         value={selectedComponenteIds}
         onChange={setSelectedComponenteIds}
+        single
       />
     </Sheet>
   )
