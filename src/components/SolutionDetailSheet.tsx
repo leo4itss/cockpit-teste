3fc5b7fae7798e9bb2cec0d10e4fc715269a11c8
leonardolins <leonardolins@users.noreply.github.com>
2026-04-29 11:20:53 -0300

    auto: update SolutionDetailSheet.tsx

--- src/components/SolutionDetailSheet.tsx
+++ src/components/SolutionDetailSheet.tsx
@@ -135,7 +135,7 @@ export function SolutionDetailSheet({ open, onClose, solution, componentes = [],
         <div className="flex flex-col gap-4">
           <SectionTitle>Informações básicas</SectionTitle>
           <div className="flex flex-col gap-4">
-            <Field label="Nome da instância da solução" value={solution.name} />
+            <Field label="Nome da instância da solução" value={solution.name} required />
             <Field label="Descrição" value={solution.description} />
             <Field label="Data de cadastro" value={solution.createdAt} />
             <Field label="Arquiteto PAS responsável" value={solution.arquitetoPAS} />
