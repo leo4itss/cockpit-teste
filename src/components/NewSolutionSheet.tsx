ef4df57e5a48f446c2b44c5c3431d514df536261
leonardolins <leonardolins@users.noreply.github.com>
2026-05-08 16:54:08 -0300

    auto: update NewSolutionSheet.tsx

--- src/components/NewSolutionSheet.tsx
+++ src/components/NewSolutionSheet.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useUsers } from '@/context/UsersContext'
 import { Sheet } from './ui/Sheet'
 import { Input } from './ui/Input'
 import { Select } from './ui/Select'
