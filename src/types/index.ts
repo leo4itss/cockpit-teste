3a7f8693bdc1b2248d898936800093807a5a0bfd
leonardolins <leonardolins@users.noreply.github.com>
2026-05-12 13:15:31 -0300

    auto: update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -173,6 +173,7 @@ export interface Contract {
   dataTermino: string
   renovacao: string
   status: 'Ativo' | 'Inativo' | 'Pendente'
+  historico?: ContractHistoricoEntry[]  // log de ajustes de licença
 }
 
 export interface User {
