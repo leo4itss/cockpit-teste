645ee0d81203b361f19fddab9af2d64daebc67e7
leonardolins <leonardolins@users.noreply.github.com>
2026-05-20 20:30:47 -0300

    auto: update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,9 +7,9 @@ export function Layout() {
   const [collapsed, setCollapsed] = useState(false)
 
   return (
-    <div className="flex min-h-screen bg-white">
+    <div className="flex h-screen bg-white overflow-hidden">
       <Sidebar collapsed={collapsed} onCollapse={setCollapsed} />
-      <div className="flex flex-col flex-1 min-w-0">
+      <div className="flex flex-col flex-1 min-w-0 min-h-0">
         <TopBar collapsed={collapsed} onExpand={() => setCollapsed(false)} />
         <main className="flex-1 overflow-auto min-h-0 flex flex-col">
           <Outlet />
