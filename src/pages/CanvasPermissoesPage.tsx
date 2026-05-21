8c994f9239647ed51aaf2c455ee898c9930218ca
leonardolins <leonardolins@users.noreply.github.com>
2026-05-21 08:35:21 -0300

    auto: update CanvasPermissoesPage.tsx

--- src/pages/CanvasPermissoesPage.tsx
+++ src/pages/CanvasPermissoesPage.tsx
@@ -690,7 +690,7 @@ export default function CanvasPermissoesPage() {
     api.getAccounts().then(acc => {
       const ativos = (acc as any[]).filter(a => !a.deletedAt)
       setAllAccounts(ativos)
-      if (!searchParams.get('account')) setAccountId(defaultAccId ?? ativos[0]?.id ?? '')
+      if (!accountId) setAccountId(defaultAccId ?? ativos[0]?.id ?? null)
     }).catch(() => {})
   }, [])
 
