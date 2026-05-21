c46029ab2419d06e915c6ef670442bd54520e6d1
leonardolins <leonardolins@users.noreply.github.com>
2026-05-21 15:55:15 -0300

    auto: update CanvasOrgPage.tsx

--- src/pages/CanvasOrgPage.tsx
+++ src/pages/CanvasOrgPage.tsx
@@ -35,7 +35,7 @@ import { useIsPlatformAdmin, useIsOrgAdmin } from '@/authz'
 import { useSessionState } from '@/hooks/useSessionState'
 import { InstanciaDetailSheet } from '@/components/instancias/InstanciaDetailSheet'
 import { AtribuirPermissoesSheet } from '@/components/permissoes/AtribuirPermissoesSheet'
-import { cn } from '@/lib/utils'
+// import { cn } from '@/lib/utils'
 import { VisualizerThemeContext, useNodeTheme, type VisualizerTheme } from '@/lib/visualizerTheme'
 import { useVisualizerTheme } from '@/hooks/useVisualizerTheme'
 
