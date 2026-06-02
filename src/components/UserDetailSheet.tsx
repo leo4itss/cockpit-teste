8a3a7893097d7a9da7f1e09be1e7f645d9f9e497
leonardolins <leonardolins@users.noreply.github.com>
2026-06-02 15:37:36 -0300

    auto: update UserDetailSheet.tsx

--- src/components/UserDetailSheet.tsx
+++ src/components/UserDetailSheet.tsx
@@ -95,7 +95,7 @@ export function UserDetailSheet({ open, onClose, user, onEdit }: Props) {
           <SectionTitle>Informações Profissionais</SectionTitle>
           <Field label="Área" value={user.area} required />
           <Field label="Cargo" value={user.cargo} required />
-          <Field label="Papel" value={user.papel} required />
+          <Field label="Nível de Acesso" value={user.papel} required />
           <Field label="Etiquetas de classificação" value={user.etiquetas} required />
         </div>
 
