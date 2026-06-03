36207966db80fa9590fcf280e9d4375a0de2058d
leonardolins <leonardolins@users.noreply.github.com>
2026-06-03 08:59:18 -0300

    auto: update UsuarioDetailAccountSheet.tsx

--- src/components/usuarios/UsuarioDetailAccountSheet.tsx
+++ src/components/usuarios/UsuarioDetailAccountSheet.tsx
@@ -122,7 +122,6 @@ export function UsuarioDetailAccountSheet({
             <SectionTitle>Informações Profissionais</SectionTitle>
             <Field label="Área" value={user.area} required />
             <Field label="Cargo" value={user.cargo} required />
-            <Field label="Perfil na empresa" value={user.papel} required />
             <Field label="Etiquetas de classificação" value={user.etiquetas} required />
           </div>
 
