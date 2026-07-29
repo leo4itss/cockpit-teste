2ccad8c251e59c5b9a162471292043972c9afe30
leonardolins <leonardolins@users.noreply.github.com>
2026-07-29 10:48:34 -0300

    auto: update EditSolutionSheet.tsx

--- src/components/EditSolutionSheet.tsx
+++ src/components/EditSolutionSheet.tsx
@@ -231,6 +231,7 @@ export function EditSolutionSheet({
     return {
       name:              s?.name              ?? '',
       description:       s?.description       ?? '',
+      accountId:         s?.accountId          ?? '',
       arquitetoPAS:      s?.arquitetoPAS      ?? '',
       marketplace:       s?.marketplace !== undefined && s?.marketplace !== null
                            ? s.marketplace !== 'Inativo'
